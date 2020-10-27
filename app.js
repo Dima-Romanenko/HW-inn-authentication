87586b9478f4cc06d300f75d1f1cbbdfdb7f1975
x3Dima <x3dima.romanenko@gmail.com>
2020-10-27 21:45:51 +0200

    added check for array length

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-
 let value = prompt("Введите ИНН");
 
 const controlNumbers = [2, 4, 10, 3, 5, 9, 4, 6, 8, 0];
 
 let dataPreparation = (data) => {
-  if (data.length === 10 && isFinite(data)) {
-    let numbers = Array.from(data).map((el) => {
-      return +el;
-    });
-    return numbers;
-  } else {
-    return;
-  }
+  let numbers = Array.from(data).map((el) => {
+    return +el;
+  });
+  return numbers;
 };
 
 let checkInn = (data, f) => {
   let inn = f(data);
   let controlSum = 0;
 
-  for (let i = 0; i < inn.length - 1; i++) {
-    controlSum += inn[i] * controlNumbers[i];
+  if (inn.length == 10 && isFinite(data)) {
+    for (let i = 0; i < inn.length - 1; i++) {
+      controlSum += inn[i] * controlNumbers[i];
+    }
   }
 
   (controlSum % 11) % 10 == inn[9] ? console.log(true) : console.log(false);
