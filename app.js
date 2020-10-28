f1186c9aa463ddc7002d795c4672d7da7b1a62ed
x3Dima <x3dima.romanenko@gmail.com>
2020-10-28 19:37:57 +0200

    add output function and styles

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let value = prompt("Введите ИНН");
 
-const controlNumbers = [2, 4, 10, 3, 5, 9, 4, 6, 8, 0];
+const controlNumbers = [-1, 5, 7, 9, 4, 6, 10, 5, 7];
+
+// Формируем массив полученных данных и приводим элементы к числу
 
 let dataPreparation = (data) => {
   let numbers = Array.from(data).map((el) => {
@@ -9,7 +11,11 @@ let dataPreparation = (data) => {
   return numbers;
 };
 
-let gender = (data) => (data[8] % 2 == 0 ? "female" : "male");
+// Функция проверки пола владельца ИНН
+
+let gender = (data) => (data[8] % 2 == 0 ? "Женский" : "Мужской");
+
+// Функция проверки  кода ИНН по формуле
 
 let checkInn = (data, f) => {
   let inn = f(data);
@@ -19,12 +25,44 @@ let checkInn = (data, f) => {
     for (let i = 0; i < inn.length - 1; i++) {
       controlSum += inn[i] * controlNumbers[i];
     }
+  } else {
+    return false;
   }
 
-  (controlSum % 11) % 10 == inn[9] ? console.log(true) : console.log(false);
-
-  console.log(gender(inn));
-  
-  console.log((controlSum % 11) % 10, inn[9]);
+  if ((controlSum % 11) % 10 == inn[9]) {
+    return true;
+  } else {
+    return false;
+  }
 };
-checkInn(value, dataPreparation);
+
+// Создаем обьект с результатом проверки
+
+let createResultObject = () => {
+  return {
+    gender: gender(value),
+    exist: checkInn(value, dataPreparation),
+  };
+};
+
+let result = createResultObject();
+
+// Вывод данных
+
+let outputBox = document.querySelector(".answer");
+
+let outputData = (res) => {
+  outputBox.innerHTML = `
+  <h3>Код ИНН - ${value}</h3>
+  `;
+  res.exist
+    ? (outputBox.innerHTML += `
+  <span>Пол: <strong>${result.gender}</strong></span></br>
+  <span>Статус: <strong>существует</strong></span>
+  `)
+    : (outputBox.innerHTML += `
+  <span>Статус: <strong>не существует</strong></span>
+  `);
+};
+
+outputData(result);
